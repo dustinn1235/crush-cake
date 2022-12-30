f5da8bd6ec5043e56625a6b30669398dda66d45b
dustinn <binhng1908@gmail.com>
2022-12-29 22:09:06 -0700

    update bulletin

--- src/components/Bulletin.js
+++ src/components/Bulletin.js
@@ -1,5 +1,6 @@
 import "../css/Bulletin.css";
-import { AiFillStar } from "react-icons/ai";
+import { TiStarFullOutline } from "react-icons/ti";
+import { FaTiktok } from "react-icons/fa";
 
 const Bulletin = () => {
   return (
@@ -7,17 +8,39 @@ const Bulletin = () => {
       <div className="bulletin-header">
         <div className="star-wrapper">
           <div className="star-wrapper-before left">
-            <AiFillStar></AiFillStar>
+            <TiStarFullOutline></TiStarFullOutline>
           </div>
         </div>
-        <div className="header-title">HOT</div>
+        <h1 className="header-title">HOT</h1>
         <div className="star-wrapper">
           <div className="star-wrapper-before right">
-            <AiFillStar></AiFillStar>
+            <TiStarFullOutline></TiStarFullOutline>
           </div>
         </div>
       </div>
-      <div className="bulletin"></div>
+      <div className="bulletin">
+        <div className="cake-banner">
+          <h1 className="outline">Custom Cake</h1>
+          <h1 className="title">Custom Cake</h1>
+          <div className="img-wrapper">
+            <img alt="pop up cake" src="./PopUpCake.png"></img>
+          </div>
+        </div>
+        <div className="discount-banner">
+          <h1 className="outline">Discount</h1>
+          <h1 className="title">Discount</h1>
+        </div>
+        <a
+          href="https://www.tiktok.com/@bunny_luv_icecream/video/7167748199275351322?is_copy_url=1&is_from_webapp=v1"
+          className="tiktok-banner"
+        >
+          <FaTiktok className="logo"></FaTiktok>
+          <div className="name-wrapper">
+            <h2>QUÁ LÀ BÁNH KEM</h2>
+            <h3>Jenny Huynh</h3>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
