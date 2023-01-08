c8c1b109eecdd71095a9411fcfe70ab15ed1c5db
dustinn <binhng1908@gmail.com>
2023-01-07 20:45:07 -0700

    update layout custom cake

--- src/components/menu/CustomCake.js
+++ src/components/menu/CustomCake.js
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import "../../css/menu/CustomCake.css";
 import Title from "../Title";
 
@@ -5,6 +6,29 @@ const CustomCake = () => {
   return (
     <div>
       <Title title="Custom Cake"></Title>
+      <div className="cakecreate-container">
+        <div id="current-step">
+          <h1>STEP 1</h1>
+          <p>CAKE'S SHAPE</p>
+        </div>
+        <div className="img-container"></div>
+        <div className="steps-container">
+          <div className="step"></div>
+          <div className="step"></div>
+          <div className="step"></div>
+          <div className="step"></div>
+          <div className="step"></div>
+          <div className="step"></div>
+        </div>
+        <div className="options-container">
+          <div className="option"></div>
+          <div className="option"></div>
+          <div className="option"></div>
+        </div>
+        <div className="button-container">
+          <Button>Done! &gt;&gt;&gt;</Button>
+        </div>
+      </div>
     </div>
   );
 };
