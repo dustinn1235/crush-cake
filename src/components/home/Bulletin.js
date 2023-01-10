3e5b4b81f601a4cb9f97ca5f280228d18b9d396b
dustinn <binhng1908@gmail.com>
2023-01-09 20:34:16 -0700

    add link to homepage

--- src/components/home/Bulletin.js
+++ src/components/home/Bulletin.js
@@ -1,6 +1,7 @@
 import "../../css/home/Bulletin.css";
 import { TiStarFullOutline } from "react-icons/ti";
 import { FaTiktok } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Bulletin = () => {
   return (
@@ -19,14 +20,14 @@ const Bulletin = () => {
         </div>
       </div>
       <div className="bulletin">
-        <div className="cake-banner">
+        <Link className="cake-banner" to="menu/customcake">
           <div className="img-wrapper">
             <img alt="pop up cake" src="./PopUpCake.png"></img>
           </div>
           <h1 className="outline">Custom Cake</h1>
           <h1 className="title">Custom Cake</h1>
-        </div>
-        <div className="discount-banner">
+        </Link>
+        <Link className="discount-banner" to="events">
           <div className="img-wrapper">
             <img
               src="./discount.png"
@@ -36,7 +37,7 @@ const Bulletin = () => {
           </div>
           <h1 className="outline">Discount</h1>
           <h1 className="title">Discount</h1>
-        </div>
+        </Link>
         <a
           href="https://www.tiktok.com/@bunny_luv_icecream/video/7167748199275351322?is_copy_url=1&is_from_webapp=v1"
           className="tiktok-banner"
