5a7d77718ab86bbd72a34609730a87d2e63f9414
dustinn <binhng1908@gmail.com>
2023-01-14 17:34:38 -0700

    payment page update

--- src/components/checkout/Payment.js
+++ src/components/checkout/Payment.js
@@ -1,9 +1,18 @@
+import { Radio } from "@mui/material";
 import { TiStarFullOutline } from "react-icons/ti";
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
+import { FaPaypal } from "react-icons/fa";
 import "../../css/checkout/Payment.css";
+import { useState } from "react";
 
 const Payment = ({ info, shipDetail, setCurStep }) => {
   const infoLabels = ["Name", "Email", "Phone", "Address", "Delivery methods"];
   const shipMethod = shipDetail === 4.99 ? "Fast" : "Express";
+  const [style, setStyle] = useState({});
+  const setMethod = () => {
+    setStyle({ transform: "scaleY(1)", height: "auto" });
+  };
 
   return (
     <div className="middle">
@@ -32,9 +41,51 @@ const Payment = ({ info, shipDetail, setCurStep }) => {
           Edit
         </p>
       </div>
-      <div className="header">
-        <TiStarFullOutline className="icon"></TiStarFullOutline>
-        <p>PAYMENT METHODS</p>
+
+      <div>
+        <div className="header">
+          <TiStarFullOutline className="icon"></TiStarFullOutline>
+          <p>PAYMENT METHODS</p>
+        </div>
+        <div className="payment-container">
+          <div>
+            <div className="payment-el method-header">
+              <Radio style={{ height: "100%" }} onClick={setMethod}></Radio>
+              <p>Credit card</p>
+              <FaCcVisa /> <FaCcMastercard />
+            </div>
+            <div className="payment-content" style={style}>
+              <div className="payment-el">
+                <input placeholder="Card number"></input>
+              </div>
+              <div className="payment-el">
+                <input placeholder="Name on card"></input>
+              </div>
+              <div className="payment-el" style={{ gap: 0 }}>
+                <input
+                  placeholder="Expiration Day (MM/YY)"
+                  style={{ width: "50%", borderRight: "0.1rem solid" }}
+                ></input>
+                <input
+                  placeholder="Security Code"
+                  style={{ width: "50%" }}
+                ></input>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="payment-el method-header">
+              <Radio style={{ height: "100%" }} onClick={setMethod}></Radio>
+              <p>Paypal</p>
+              <FaPaypal />
+            </div>
+            <div className="payment-content" style={style}>
+              <div className="payment-el">
+                <input placeholder="Card number"></input>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
