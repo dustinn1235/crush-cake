0ef7ea867c1c8340e4386a38ecb4a1700330dbc7
dustinn <binhng1908@gmail.com>
2023-01-21 22:05:04 -0700

    fixing mobile blog1

--- src/components/home/AboutUs.js
+++ src/components/home/AboutUs.js
@@ -12,11 +12,14 @@ const AboutUs = () => {
           <p>
             &nbsp;&nbsp;&nbsp;&nbsp;Crush is a brand that was born with the
             desire to serve the needs of customers to desgin a meaningful gift
-            for their lvoed ones or themselves. Each gift has the mission of
-            sending good wishes to the recipient. Not only focusing on the
-            appearance of the gift, we also pay special attention to the taste
-            of the product. Each cake is aimed at the health of consumers,
-            without using preservatives or additives harmful to health.
+            for their loved ones or themselves. Each gift has the mission of
+            sending good wishes to the recipient.
+            <span>
+              Not only focusing on the appearance of the gift, we also pay
+              special attention to the taste of the product. Each cake is aimed
+              at the health of consumers, without using preservatives or
+              additives harmful to health.
+            </span>
           </p>
           <Link style={{ marginLeft: "auto" }} to="about">
             <button>
